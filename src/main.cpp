9f340125357f167c81237231110259bc0a64de8b
Jaidon Lybbert <jaidonlybbert@gmail.com>
2023-12-06 16:50:29 -0800

    Added project config header, and main()

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "EngineConfig.h"
+
+using namespace std;
+
+int main() {
+    cout << "Engine Version: " << Engine_VERSION_MAJOR << "." 
+         << Engine_VERSION_MINOR << endl;
+    return 0;
+}
