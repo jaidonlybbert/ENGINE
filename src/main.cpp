85ff3cab24a67e96e017646d04fd15ed726676e5
Jaidon Lybbert <jaidonlybbert@gmail.com>
2023-12-11 15:52:15 -0800

    Vulkan-tutorial.com Setup/Instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,91 @@
 #include<iostream>
+#include<vector>
 #include<stdexcept>
 #include<cstdlib>
-#include "GLFW/glfw3.h"
 #include "vulkan/vulkan.hpp"
+#include "GLFW/glfw3.h"
 #include "EngineConfig.h"
 
 using namespace std;
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class VulkanTemplateApp {
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
 private:
+	GLFWwindow* window;
+	VkInstance instance;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
 	void initVulkan() {
 
 	}
 
-	void mainLoop() {
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Vulkan Template";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
 
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		// Query Vulkan extensions required by GLFW3
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+		
+		// Query available extensions
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "Available Vulkan Extensions:" << endl;
+
+		for (const auto& extension: extensions) {
+			std::cout << '\t' << extension.extensionName << endl;
+		}
+
+		// Create instance
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
+
+	void mainLoop() {
+		while(!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup() {
-
+		vkDestroyInstance(instance, nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
 	}
 };
 
