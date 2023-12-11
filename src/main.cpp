171a439d07626feb453ef2abcd6b2b6552873d4f
Jaidon Lybbert <jaidonlybbert@gmail.com>
2023-12-10 23:22:20 -0800

    Started VulkanTemplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,46 @@
 #include<iostream>
+#include<stdexcept>
+#include<cstdlib>
 #include "GLFW/glfw3.h"
 #include "vulkan/vulkan.hpp"
 #include "EngineConfig.h"
 
 using namespace std;
 
+class VulkanTemplateApp {
+public:
+	void run() {
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	void initVulkan() {
+
+	}
+
+	void mainLoop() {
+
+	}
+
+	void cleanup() {
+
+	}
+};
+
 int main() {
     cout << "Engine Version: " << Engine_VERSION_MAJOR << "." 
          << Engine_VERSION_MINOR << endl;
-    return 0;
+
+    VulkanTemplateApp app;
+
+    try {
+	    app.run();
+    } catch (const std::exception& e) {
+	    std::cerr << e.what() << std::endl;
+	    return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
