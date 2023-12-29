7c8de669b881d5aa60e2c9d142d6108df2798616
Jaidon Lybbert <jaidonlybbert@gmail.com>
2023-12-29 09:51:06 -0800

    Created CMake installer build w/ NSIS

--- src/main.cpp
+++ src/main.cpp
@@ -54,6 +54,7 @@ static std::vector<char> readFile(const std::string& filename) {
 	std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
 	if (!file.is_open()) {
+		std::cout << "no open not debug lol";
 		throw std::runtime_error("failed to open file!");
 	}
 
