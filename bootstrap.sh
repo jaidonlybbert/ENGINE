9b92554507b5ea21e1f2fe36dc6442c6f027210d
Jaidon Lybbert <jaidonlybbert@gmail.com>
2024-07-02 15:04:58 -0700

    Fixed CMake build to wrong directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
-cmake -G "Ninja" -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang .
-cmake --build .\build --config Release
-.\build\Release\Engine.exe
+cd build
+cmake -G "Ninja" -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang ..
+cmake --build . --config Release
+.\Release\Engine
