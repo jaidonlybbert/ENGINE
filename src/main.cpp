5bcaa5b95bd891929be33da4e111db9170a798b0
Jaidon Lybbert <jaidonlybbert@gmail.com>
2025-01-25 12:46:45 -0800

    Fixed tinygltf includes

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,12 @@
 #include "vulkan/vulkan.hpp"
 #include "GLFW/glfw3.h"
 #include "EngineConfig.h"
-#include "Boost/filesystem.hpp"
+#include "boost/filesystem.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image.h>
+#include <stb_image_write.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
@@ -36,6 +38,9 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_vulkan.h"
 
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
 #include "tiny_gltf.h"
 
 const uint32_t WIDTH = 800;
