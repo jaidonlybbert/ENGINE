ad6245ec1adc7d3ba1d8c2f657d01b81749e813f
Jaidon Lybbert <jaidonlybbert@gmail.com>
2025-12-13 21:13:20 -0800

    Moved scene generation to new 'scenes' directory

---
+++ src/scenes/SceneWorld.hpp
@@ -0,0 +1,6 @@
+#include "ProceduralGeometry.h"
+
+void create_world_polyhedra(VulkanTemplateApp& app);
+void addBoundingBoxChild(ENG::Node* node, VulkanTemplateApp& app, const std::string &bbName);
+void create_tetrahedron_no_pmp(VulkanTemplateApp& app);
+void initializeWorldScene(VulkanTemplateApp& app);
