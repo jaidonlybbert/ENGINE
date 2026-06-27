7fabfd42f766eb482bbead6b007952f566da125a
jaidonlybbert <jaidonlybbert@gmail.com>
2026-06-27 10:08:13 -0700

    add namespace to node

--- src/scene/Node.cpp
+++ src/scene/Node.cpp
@@ -1,20 +1,22 @@
 #include "scene/Node.hpp"
 
-void set_property(Node2& node, const property_t propertyFlag)
-{
-	node.propertyFlags |= propertyFlag;
-}
-
-bool has_property(const Node2& node, const property_t propertyFlag)
-{
-	return ((node.propertyFlags && propertyFlag) != 0);
-}
-
-void depth_first_traverse(node_array_t& nodes)
-{
-	/*
-	for (auto& node : nodes) {
-		nodeStack.push(node);
+namespace ENG {
+	void set_property(Node2& node, const property_t propertyFlag)
+	{
+		node.propertyFlags |= propertyFlag;
 	}
-	*/
-};
+
+	bool has_property(const Node2& node, const property_t propertyFlag)
+	{
+		return ((node.propertyFlags && propertyFlag) != 0);
+	}
+
+	void depth_first_traverse(node_array_t& nodes)
+	{
+		/*
+		for (auto& node : nodes) {
+			nodeStack.push(node);
+		}
+		*/
+	};
+}
